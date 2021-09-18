80c9e7e7e013e4ed4cf7fbda9f2472f8be5505e9
labib1235 <86480926+labib1235@users.noreply.github.com>
2021-09-18 13:16:16 +0600

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 99;
+        int b = 45;
+
+        int addition = a + b;
+        int subtraction = a - b;
+        int multiplication = a * 4;
+        int division = a / 9;
+
+        System.out.println("Addition: " + addition);
+        System.out.println("subtraction: " + subtraction);
+        System.out.println("multiplication: " + multiplication);
+        System.out.println("division: " + division);
+    }
+}
