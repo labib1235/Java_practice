b9b8ec585c2c44dd36e748065f9a50a9765a9a94
labib1235 <86480926+labib1235@users.noreply.github.com>
2021-09-18 13:01:05 +0600

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,12 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        a = 26;
+        b = 32;
+        int result;
+        result = a + b;
+
+        System.out.println("The result is: "+ result);
+    }
+}
