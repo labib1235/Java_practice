41704f1996cd0b8fbc72d1aae06e5b3c831188ea
labib1235 <86480926+labib1235@users.noreply.github.com>
2021-12-07 20:50:30 +0600

    Reading of items of a table of java database

--- Main.java
+++ Main.java
@@ -1,6 +1,37 @@
-public class Main {
-    public static void main(String[] args) {
+package com.company;
 
-        System.out.println("Hello world!");
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String URL = "jdbc:mysql://localhost/labib?serverTimezone=UTC";
+        String USER_NAME = "root";
+        String PASSWORD = "";
+
+        try {
+            Connection con = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+
+            String sql = "select name as p_name, id as p_id, age as p_age, address as p_address from person_info";
+
+            Statement statement = con.createStatement();
+
+            ResultSet resultSet = statement.executeQuery(sql);
+
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("p_id");
+                String name = resultSet.getString("p_name");
+                int age = resultSet.getInt("p_age");
+                String address = resultSet.getString("p_address");
+
+                System.out.println("id: " + id + " name: " + name + " age: " + age + " address: " + address);
+
+            }
+
+        con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
