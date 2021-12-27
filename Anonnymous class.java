19fb081f7a6691ffac6c8052954f10ce20a26cd1
labib1235 <86480926+labib1235@users.noreply.github.com>
2021-12-27 12:27:09 +0600

    Anonnymous class in java

---
+++ Anonnymous class.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Anonnymous class
+        Printer printer = new Printer() {
+            @Override
+            public void print() {
+
+            }
+
+            @Override
+            public void scan() {
+
+            }
+        };
+
+        printer.print();
+
+
+    }
+}
+
+interface Printer {
+    void print();
+    void scan();
+}
